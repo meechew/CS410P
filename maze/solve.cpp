c5bda29b8720efea2dfadf0e09112c46327f2b16
meechew <cbunt@pdx.edu>
2020-04-15 20:03:01 -0700

    Init

---
+++ maze/solve.cpp
@@ -0,0 +1,128 @@
+#include "maze.h"
+#include "path.h"
+#include<queue>
+#include<vector>
+#include<list>
+#include<tuple>
+#include<utility>
+#include<iostream>
+#include<climits>
+#include<sstream>
+using namespace std;
+
+path solve_dfs(Maze& m, int rows, int cols);
+path solve_bfs(Maze& m, int rows, int cols);
+path solve_dijkstra(Maze& m, int rows, int cols);
+path solve_tour(Maze& m, int rows, int cols);
+
+
+int main(int argc, char** argv)
+{
+    if(argc != 4)
+    {
+        cerr << "usage:\n"
+             << "./maze option rows cols\n"
+             << " options:\n"
+             << "  -dfs: depth first search (backtracking)\n"
+             << "  -bfs: breadth first search\n"
+             << "  -dij: dijkstra's algorithm\n"
+             << "  -tour: all corners tour\n"
+             << "  -basic: run dfs, bfs, and dij\n"
+             << "  -advanced: run dfs, bfs, dij and tour" << endl;
+        return 0;
+    }
+    string opt(argv[1]);
+
+    int rows, cols;
+    stringstream s;
+    s << argv[2] << " " << argv[3];
+    s >> rows >> cols;
+
+    // construct a new random maze;
+    Maze m(rows, cols);
+
+    // print the initial maze out
+    cout << "Initial maze" << endl;
+    m.print_maze(cout, opt == "-dij" || opt == "-tour");
+
+    if(opt == "-dfs")
+    {
+        cout << "\nSolved dfs" << endl;
+        path p = solve_dfs(m, rows, cols);
+        m.print_maze_with_path(cout, p, false, false);
+    }
+
+
+    if(opt == "-bfs")
+    {
+        cout << "\nSolved bfs" << endl;
+        path p = solve_bfs(m, rows, cols);
+        m.print_maze_with_path(cout, p, false, false);
+    }
+
+    if(opt == "-dij")
+    {
+        cout << "\nSolved dijkstra" << endl;
+        path p = solve_dijkstra(m, rows, cols);
+        m.print_maze_with_path(cout, p, true, false);
+    }
+
+    if(opt == "-tour")
+    {
+        cout << "\nSolved all courners tour" << endl;
+        path p = solve_tour(m, rows, cols);
+        m.print_maze_with_path(cout, p, true, true);
+    }
+    if(opt == "-basic")
+    {
+        cout << "\nSolved dfs" << endl;
+        path p = solve_dfs(m, rows, cols);
+        m.print_maze_with_path(cout, p, false, false);
+
+        cout << "\nSolved bfs" << endl;
+        p = solve_bfs(m, rows, cols);
+        m.print_maze_with_path(cout, p, false, false);
+
+        cout << "\nSolved dijkstra" << endl;
+        p = solve_dijkstra(m, rows, cols);
+        m.print_maze_with_path(cout, p, true, false);
+    }
+    if(opt == "-advanced")
+    {
+        cout << "\nSolved dfs" << endl;
+        path p = solve_dfs(m, rows, cols);
+        m.print_maze_with_path(cout, p, false, false);
+
+        cout << "\nSolved bfs" << endl;
+        p = solve_bfs(m, rows, cols);
+        m.print_maze_with_path(cout, p, false, false);
+
+        cout << "\nSolved dijkstra" << endl;
+        p = solve_dijkstra(m, rows, cols);
+        m.print_maze_with_path(cout, p, true, false);
+
+        cout << "\nSolved all courners tour" << endl;
+        p = solve_tour(m, rows, cols);
+        m.print_maze_with_path(cout, p, true, true);
+    }
+}
+
+path solve_dfs(Maze& m, int rows, int cols)
+{
+    return list<point>();
+}
+
+path solve_bfs(Maze& m, int rows, int cols)
+{
+    return list<point>();
+}
+
+path solve_dijkstra(Maze& m, int rows, int cols)
+{
+    return list<point>();
+}
+
+path solve_tour(Maze& m, int rows, int cols)
+{
+    return list<point>();
+}
