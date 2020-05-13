74866a6b175c988eb19e2ff5afff45d40b52ad33
meechew <cbunt@pdx.edu>
2020-05-12 21:04:40 -0700

    Well. on our way.

--- term/term.hpp
+++ term/term.hpp
@@ -36,20 +36,119 @@ using rule = std::pair<term_ptr<T>, term_ptr<T>>;
 /////////////////////////////////////////////////////////////////
 
 template<typename T>
+class term_iterator {
+private:
+  std::vector<std::shared_ptr<term<T>>> Pos;
+public:
+  typedef term<T> T_Type;
+  typedef term<T> *T_Ptr;
+  typedef term<T> &T_Ref;
+  typedef size_t T_Size;
+  typedef ptrdiff_t  T_dif;
+  typedef std::bidirectional_iterator_tag iterator_category;
+
+  term_iterator<T>() = delete;
+  term_iterator<T>(const term_iterator<T> &i): Pos(i.Pos) {}
+  term_iterator<T>(std::shared_ptr<term<T>> p) {
+    Pos.push_back(p);
+  }
+  T& operator*() const {
+    return Pos.front()->value;
+  }
+
+  T* operator->() const {
+    return &Pos.front()->value;
+  }
+  term_iterator& operator++();
+  term_iterator& operator--();
+
+  term_iterator operator++(int){
+    term_iterator<T> tmp(*this);
+    ++this;
+    return tmp;
+  }
+
+  term_iterator operator--(int) {
+    term_iterator<T> tmp(*this);
+    --this;
+    return tmp;
+  }
+
+  term_iterator& operator-=(unsigned int n) {
+    for(int i = 0; i < n; i++) ++*this;
+    return *this;
+  }
+
+  term_iterator& operator+=(unsigned int n) {
+    for(int i = 0; i < n; i++) --*this;
+    return *this;
+  }
+
+  bool operator==(const term_iterator<T>& Next) const
+  {
+    return Pos == Next.Pos;
+  }
+
+  bool operator!=(const term_iterator<T>& Next) const
+  {
+    return Pos != Next.Pos;
+  }
+};
+
+template<typename T>
+term_iterator<T>& term_iterator<T>::operator++() {
+  if(!Pos.empty()){
+    std::vector<int>::iterator it = Pos.begin();
+    return ++it;
+  }
+  return NULL;
+}
+
+template<typename T>
+term_iterator<T>& term_iterator<T>::operator--() {
+  if(!Pos.empty()){
+    std::vector<int>::iterator it = Pos.end();
+    return --it;
+  }
+  return NULL;
+}
+
+
+
+template<typename T>
 class term {
 private:
   typedef term<T> T_Type;
   typedef term<T> *T_Ptr;
   typedef term<T> &T_Ref;
   typedef size_t T_Size;
-  typedef prtdiff_t T_dif;
-  
+  typedef ptrdiff_t  T_dif;
+
+  typedef std::bidirectional_iterator_tag iterator_category;
   typedef term_iterator<T> iterator;
   typedef term_iterator<const T> const_iterator;
 
+  typedef std::reverse_iterator<iterator>       reverse_iterator;
+  typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
+
   std::ostream &operator<<(std::ostream &out);
 public:
+  term() {};
 
+
+  iterator begin() {return term_iterator<T>();}
+  iterator end() {return term_iterator<T>();}
+  const_iterator cbegin()   {return term_iterator<const T>();}
+  const_iterator cend()     {return term_iterator<const T>();}
+
+  reverse_iterator rbegin() {return std::reverse_iterator<iterator>(end());}
+  reverse_iterator rend()   {return std::reverse_iterator<iterator>(begin());}
+  const_reverse_iterator crbegin()
+  {return std::reverse_iterator<const_iterator>(cend());}
+  const_reverse_iterator crend()
+  {return std::reverse_iterator<const_iterator>(cbegin());}
+
+  virtual void print(std::ostream &out) = 0;
 };
 
 template<typename T>
@@ -89,10 +188,11 @@ private:
   std::vector<std::shared_ptr<term<T>>> Argv;
 public:
   function(){};
-  function(std::string type, int argc, std::vector<std::shared_ptr<term<T>>> argv){
-
+  function(std::string type, int argc, std::vector<std::shared_ptr<term<T>>> argv)
+  : Type(type), Argc(argc), Argv(argv){}
+  void print(std::ostream &out) {
+    out << Type;
   }
-
 };
 
 
@@ -113,7 +213,7 @@ bool unify(const term<T>& t1, const term<T>& t2, Sub<T>& sigma)
 //
 /////////////////////////////////////////////////////////////////
 
-Template<typename T>
+template<typename T>
 term_ptr<T> reduce(term_ptr<T> t, const std::vector<rule<T>>& rules)
 {
 }
@@ -139,48 +239,5 @@ template<typename T>
 std::ostream& operator<<(std::ostream& out, const term<T>& t)
 {
 }
-
-
-template<typename T>
-class term_iterator {
-private:
-  std::vector<std::shared_ptr<term<T>>> Pos;
-public:
-  term_iterator<T>() {}
-  term_iterator<T>(const term_iterator<T> &i): Pos(i.Pos) {}
-  term_iterator<T>(std::shared_ptr<term<T>> p) {
-    Pos.push_back(p);
-  }
-  T& operator*() const;
-  T* operator->() const;
-  term_iterator& operator++();
-  term_iterator& operator--();
-
-  term_iterator operator++(int){
-    tree_iterator<T> tmp(*this);
-    ++this;
-    return tmp;
-  }
-
-  term_iterator operator--(int) {
-    tree_iterator<T> tmp(*this);
-    --this;
-    return tmp;
-  }
-  
-
-  term_iterator& operator+=(unsigned int n) {
-    for(int i = 0; i < n; i++) --*this;
-    return *this;
-  }
-  
-  term_iterator& operator-=(unsigned int n) {
-    for(int i = 0; i < n; i++) ++*this;
-    return *this;
-  }
-
-
-};
-
 
 #endif // TERM_HPP
--- term/term_iterator.cpp
+++
@@ -1,3 +0,0 @@
-// Created by CBunt on 10 May 2020.
-//
-
